28b752190aa7bac5df368464a04a9b170669f02b
Yorvin Rosario <yorvin.rosario@axa-equitable.com>
2019-04-16 17:05:17 -0400

    added image uploading

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let express = require('express');
 let app = new express();
 let mongoose = require('mongoose');
 let bodyParser = require('body-parser');
+let fileUpload = require('express-fileupload');
+
 let Post = require('./database/models/Post');
 
 app.use(express.static('public'));
@@ -12,12 +14,11 @@ mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true 
   .then(() => 'You are now connected to Mongo!')
   .catch(err => console.error('Something went wrong', err));
 
+app.use(fileUpload());
 app.use(expressEdge);
 app.set('views', __dirname + '/views');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', async (req, res) => {
   let posts = await Post.find({});
@@ -36,8 +37,14 @@ app.get('/post', (req, res) => {
     res.render('create');
   });
   app.post('/posts/store', (req, res) => {
-    Post.create(req.body, (error, post) => {
-      res.redirect('/')
+    let { image } = req.files;
+    image.mv(path.resolve(__dirname, 'public/posts', image.name), (error) => {
+      Post.create({
+        ...req.body,
+        image: `/posts/${image.name}`
+      }, (error, post) => {
+        res.redirect('/');
+      })
     })
   });
   app.get('/post/:id', async (req, res) => {
