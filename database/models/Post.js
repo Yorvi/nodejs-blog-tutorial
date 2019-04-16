51a5f7f5e0f5202115f9ff904dce58c5a3f635ff
Yorvin Rosario <yorvin.rosario@axa-equitable.com>
2019-04-16 16:53:01 -0400

    added mongodb posts to show

--- database/models/Post.js
+++ database/models/Post.js
@@ -3,7 +3,12 @@ let mongoose = require('mongoose');
 let PostSchema = new mongoose.Schema({
   title: String,
   description: String,
-  content: String
+  content: String,
+  username: String,
+  createdAt: {
+    type: Date,
+    default: new Date()
+  }
 });
 
 let Post = mongoose.model('Post', PostSchema);
--- index.js
+++ index.js
@@ -3,18 +3,25 @@ let expressEdge = require('express-edge');
 let express = require('express');
 let app = new express();
 let mongoose = require('mongoose');
+let bodyParser = require('body-parser');
+let Post = require('./database/models/Post');
 
 app.use(express.static('public'));
 
 mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true })
   .then(() => 'You are now connected to Mongo!')
-  .catch(err => console.error('Something went wrong', err))
+  .catch(err => console.error('Something went wrong', err));
 
 app.use(expressEdge);
 app.set('views', __dirname + '/views');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  let posts = await Post.find({});
+  res.render('index', { posts });
 });
 app.get('/about', (req, res) => {
   res.render('about');
@@ -28,6 +35,15 @@ app.get('/post', (req, res) => {
   app.get('/posts/new', (req, res) => {
     res.render('create');
   });
+  app.post('/posts/store', (req, res) => {
+    Post.create(req.body, (error, post) => {
+      res.redirect('/')
+    })
+  });
+  app.get('/post/:id', async (req, res) => {
+    let post = await Post.findById(req.params.id)
+    res.render('post', { post })
+  })
 
 app.listen(4000, () => {
   console.log('App listening on port 4000');
